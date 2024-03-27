fe0be9faf49e6b7f1507dd48c83938274f340950
Peyton-McKee <mckee.p@northeastern.edu>
2024-03-27 18:37:49 -0400

    Debug Wheel Output

--- NERODevelopment/src/models/raspberry_model.cpp
+++ NERODevelopment/src/models/raspberry_model.cpp
@@ -263,7 +263,7 @@ std::optional<bool> RaspberryModel::getRightButtonPressed() {
   std::optional<float> value = this->getById(RIGHTBUTTON);
   if (value) {
     std::string binary = std::bitset<8>(static_cast<int>(*value)).to_string();
-    return binary.length() >= 2 ? binary[1] == 1 : false;
+    return binary.length() >= 3 ? binary[2] == 1 : false;
   }
   return std::nullopt;
 }
@@ -299,7 +299,7 @@ std::optional<bool> RaspberryModel::getHomeButtonPressed() {
   std::optional<float> value = this->getById(HOMEBUTTON);
   if (value) {
     std::string binary = std::bitset<8>(static_cast<int>(*value)).to_string();
-    return binary.length() >= 1 ? binary[1] == 1 : false;
+    return binary.length() >= 2 ? binary[1] == 1 : false;
   }
   return std::nullopt;
 }
