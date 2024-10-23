bb13843a9939fb56c52114e079095df05d6ccbf8
lilyshiomitsu <lilyshiomitsu@gmail.com>
2024-10-23 17:41:23 -0400

    #110: update snake controller

---
+++ NERODevelopment/src/controllers/snakecontroller.cpp
@@ -0,0 +1,46 @@
+#include "snakecontroller.h"
+
+SnakeController::SnakeController(Model *model, QObject *parent)
+    : ButtonController{model, 6, parent} {
+  // this->m_debounceOffset = 150;
+}
+
+void SnakeController::handleKeyPress(int key) {
+  int newDirection = m_currentDirection;
+
+  switch (key) {
+  case Qt::Key_Up:
+    newDirection = 0;
+    break;
+
+  case Qt::Key_Right:
+    newDirection = 1;
+    break;
+
+  case Qt::Key_Down:
+    newDirection = 2;
+    break;
+
+  case Qt::Key_Left:
+    newDirection = 3;
+    break;
+
+  default:
+    return;
+  }
+
+  if (!isOppositeDirection(newDirection)) {
+    m_currentDirection = newDirection;
+    emit directionChanged(m_currentDirection);
+  }
+}
+
+bool SnakeController::isOppositeDirection(int newDirection) {
+  return (m_currentDirection + 2) % 4 == newDirection;
+}
+
+void SnakeController::saveScore(int score) {
+  QString topic = "NERO/SNAKE/SCORE";
+  QString message = QString::number(score);
+  m_model->sendMessage(topic, message);
+}
