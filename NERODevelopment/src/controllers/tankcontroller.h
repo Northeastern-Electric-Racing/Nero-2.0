1b7170b479a07dd7d3c8ab6101a7529a50736998
jessicazha0 <zhao.je@northeastern.edu>
2025-02-10 16:00:06 -0500

    forgot to save code

--- NERODevelopment/src/controllers/tankcontroller.h
+++ NERODevelopment/src/controllers/tankcontroller.h
@@ -1,4 +1,26 @@
 #ifndef TANKCONTROLLER_H
 #define TANKCONTROLLER_H
 
+#include "buttoncontroller.h"
+#include <QObject>
+
+class TankController : public ButtonController {
+  Q_OBJECT
+
+public:
+  explicit TankController(Model *model, QObject *parent = nullptr);
+
+public slots:
+  void handleKeyPress(int key);
+  void saveScore(int);
+
+signals:
+  void barrelAngleChanged(int newBarrelAngle);
+  void positionChanged(int newPosition);
+
+private:
+  int m_barrelAngle = 0;
+  int m_position;
+};
+
 #endif // TANKCONTROLLER_H
