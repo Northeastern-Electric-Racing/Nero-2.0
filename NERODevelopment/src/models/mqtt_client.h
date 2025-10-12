99a4e56af6f654c151557a0edf102956a376a83b
Dev Chechi <chechi.d@northeastern.edu>
2025-10-12 17:08:25 -0400

    Implemented ENV file Co-authored-by: Kavinraj-J <kavinraj.jayaraj@gmail.com>

--- NERODevelopment/src/models/mqtt_client.h
+++ NERODevelopment/src/models/mqtt_client.h
@@ -84,7 +84,7 @@ private slots:
 private:
   QMqttClient *m_client;
   QMqttSubscription *m_sub;
-  QString hostname = "localhost";
+  QString hostname = getenv("HOST") ? getenv("HOST") : "localhost";
   int m_port = 1883;
   qint8 QoS = 0;
   QString default_topic = "#";
