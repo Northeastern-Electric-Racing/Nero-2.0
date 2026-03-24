485a03ec9d68045b266397b82a28a79b7aa9cdcf
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-03-24 00:17:37 -0400

    format

--- NERODevelopment/src/controllers/homecontroller.cpp
+++ NERODevelopment/src/controllers/homecontroller.cpp
@@ -2,85 +2,85 @@
 
 HomeController::HomeController(Model *model, QObject *parent)
     : ButtonController{model, {1, 2}, parent}, m_speed(0), m_status(false),
-    m_direction(true), m_packTemp(0.0), m_motorTemp(0.0),
-    m_stateOfCharge(0.0) {
-    connect(m_model, &Model::onCurrentDataChange, this,
-            &HomeController::currentDataDidChange);
+      m_direction(true), m_packTemp(0.0), m_motorTemp(0.0),
+      m_stateOfCharge(0.0) {
+  connect(m_model, &Model::onCurrentDataChange, this,
+          &HomeController::currentDataDidChange);
 }
 
 int HomeController::speed() const { return m_speed; }
 
 void HomeController::setSpeed(int speed) {
-    if (m_speed != speed) {
-        m_speed = speed;
-        emit speedChanged(speed);
-    }
+  if (m_speed != speed) {
+    m_speed = speed;
+    emit speedChanged(speed);
+  }
 }
 
 bool HomeController::status() const { return m_status; }
 
 void HomeController::setStatus(bool status) {
-    if (m_status != status) {
-        m_status = status;
-        emit statusChanged(status);
-    }
+  if (m_status != status) {
+    m_status = status;
+    emit statusChanged(status);
+  }
 }
 
 bool HomeController::direction() const { return m_direction; }
 
 void HomeController::setDirection(bool direction) {
-    if (m_direction != direction) {
-        m_direction = direction;
-        emit directionChanged(direction);
-    }
+  if (m_direction != direction) {
+    m_direction = direction;
+    emit directionChanged(direction);
+  }
 }
 
 float HomeController::packTemp() const { return m_packTemp; }
 
 void HomeController::setPackTemp(float packTemp) {
-    if (m_packTemp != packTemp) {
-        m_packTemp = packTemp;
-        emit packTempChanged(packTemp);
-    }
+  if (m_packTemp != packTemp) {
+    m_packTemp = packTemp;
+    emit packTempChanged(packTemp);
+  }
 }
 
 float HomeController::motorTemp() const { return m_motorTemp; }
 
 void HomeController::setMotorTemp(float motorTemp) {
-    if (m_motorTemp != motorTemp) {
-        m_motorTemp = motorTemp;
-        emit motorTempChanged(motorTemp);
-    }
+  if (m_motorTemp != motorTemp) {
+    m_motorTemp = motorTemp;
+    emit motorTempChanged(motorTemp);
+  }
 }
 
 int HomeController::stateOfCharge() const { return m_stateOfCharge; }
 
 void HomeController::setStateOfCharge(int charge) {
-    if (charge != m_stateOfCharge) {
-        m_stateOfCharge = charge;
-        emit stateOfChargeChanged(charge);
-    }
+  if (charge != m_stateOfCharge) {
+    m_stateOfCharge = charge;
+    emit stateOfChargeChanged(charge);
+  }
 }
 
 int HomeController::lowVoltageStateOfCharge() const {
-    return m_lowVoltageStateOfCharge;
+  return m_lowVoltageStateOfCharge;
 }
 
 void HomeController::setLowVoltageStateOfCharge(int charge) {
-    if (charge != m_lowVoltageStateOfCharge) {
-        m_lowVoltageStateOfCharge = charge;
-        emit lowVoltageStateOfChargeChanged(charge);
-    }
+  if (charge != m_lowVoltageStateOfCharge) {
+    m_lowVoltageStateOfCharge = charge;
+    emit lowVoltageStateOfChargeChanged(charge);
+  }
 }
 
 void HomeController::currentDataDidChange() {
-    if (this->m_pageIndices.contains(this->m_model->currentPageIndex)) {
-        setPackTemp(*m_model->getPackTemp());
-        setMotorTemp(*m_model->getMotorTemp());
-        setDirection(*m_model->getDir());
-        setStateOfCharge(*m_model->getStateOfCharge());
-        setSpeed(*m_model->getMph());
-        setStatus(*m_model->getStatus());
-        setLowVoltageStateOfCharge(*m_model->getLowVoltageStateOfCharge());
-    }
+  if (this->m_pageIndices.contains(this->m_model->currentPageIndex)) {
+    setPackTemp(*m_model->getPackTemp());
+    setMotorTemp(*m_model->getMotorTemp());
+    setDirection(*m_model->getDir());
+    setStateOfCharge(*m_model->getStateOfCharge());
+    setSpeed(*m_model->getMph());
+    setStatus(*m_model->getStatus());
+    setLowVoltageStateOfCharge(*m_model->getLowVoltageStateOfCharge());
+  }
 }
