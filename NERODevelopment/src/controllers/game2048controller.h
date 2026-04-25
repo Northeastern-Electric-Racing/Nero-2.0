8b345f5e1b9f1386b132fe07d4fe6fff470e2083
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2026-04-25 00:02:58 -0400

    2048 saves

--- NERODevelopment/src/controllers/game2048controller.h
+++ NERODevelopment/src/controllers/game2048controller.h
@@ -3,22 +3,32 @@
 
 #include "buttoncontroller.h"
 #include <QObject>
+#include <QVariantList>
 
 class Game2048Controller : public ButtonController {
   Q_OBJECT
   Q_PROPERTY(bool gameOver READ gameOver WRITE setGameOver NOTIFY
                  gameOverChanged FINAL)
   Q_PROPERTY(int score READ score WRITE setScore NOTIFY scoreChanged FINAL)
+  Q_PROPERTY(int bestScore READ bestScore WRITE setBestScore NOTIFY
+                 bestScoreChanged FINAL)
 
 public:
   explicit Game2048Controller(Model *model, QObject *parent = nullptr);
 
   bool gameOver() const;
   int score() const;
+  int bestScore() const;
+
+  Q_INVOKABLE bool hasSavedGame() const;
+  Q_INVOKABLE QVariantList savedBoard() const;
+  Q_INVOKABLE bool savedHasWon() const;
+  Q_INVOKABLE void saveState(const QVariantList &board, bool hasWon);
 
 public slots:
   void setGameOver(bool gameOver);
   void setScore(int score);
+  void setBestScore(int bestScore);
   void enterButtonPressed() override;
   void upButtonPressed() override;
   void downButtonPressed() override;
@@ -29,12 +39,17 @@ public slots:
 signals:
   void gameOverChanged();
   void scoreChanged();
+  void bestScoreChanged();
   void moveMade(int direction);
   void restartRequested();
 
 private:
   bool m_gameOver = true;
   int m_score = 0;
+  int m_bestScore = 0;
+  QVariantList m_savedBoard;
+  bool m_savedHasWon = false;
+  bool m_hasSavedGame = false;
 };
 
 #endif // GAME2048CONTROLLER_H
