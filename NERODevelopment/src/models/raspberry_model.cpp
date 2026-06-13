c326df4d5393a259537e418006b3f8693df4c310
Jack Rubacha <rubacha.jack03@gmail.com>
2026-06-13 02:14:52 -0400

    Get rid of 2nd client

--- NERODevelopment/src/models/raspberry_model.cpp
+++ NERODevelopment/src/models/raspberry_model.cpp
@@ -94,14 +94,17 @@ void RaspberryModel::connectToMQTT() {
       CRITICALFAULTS,
       NONCRITICALFAULTS,
       LVVOLTAGE,
-
+      BUTTONID,
+      HOMEBUTTON,
+      MODEINDEX,
+      DIRECTION,
   };
 
   const char *client1_port_str = getenv("CLIENT1_PORT");
-  const char *client2_port_str = getenv("CLIENT2_PORT");
+  //const char *client2_port_str = getenv("CLIENT2_PORT");
 
   int client1_port = client1_port_str ? atoi(client1_port_str) : 1883;
-  int client2_port = client2_port_str ? atoi(client2_port_str) : 1882;
+  //int client2_port = client2_port_str ? atoi(client2_port_str) : 1882;
 
   MqttClient *client_1 =
       new MqttClient(nullptr, client1_port, client_1_topics, mqttHost);
@@ -110,10 +113,7 @@ void RaspberryModel::connectToMQTT() {
   client_1->connectToHost();
 
   QList<QString> client_2_topics = {
-      BUTTONID,
-      HOMEBUTTON,
-      MODEINDEX,
-      DIRECTION,
+
   };
   MqttClient *client_2 =
       new MqttClient(nullptr, client2_port, client_2_topics, mqttHost);
